355ad77a9c15bdfdff392a44e6220cb43152e0d8
felixDev22 <felixnzomo@gmail.com>
2023-02-28 11:25:04 +0300

    Update data

--- data.sql
+++ data.sql
@@ -1,13 +1,13 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, neutered, escape_attempts, weight_Kg ); 
-VALUES (1, 'Agumon', '03-02-2020', TRUE, 0, 10.23);
+INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_Kg )
+VALUES (1, 'Agumon', '2020-02-03', true, 0, 10.23);
 
-INSERT INTO animals (name, date_of_birth, neutered, escape_attempts, weight_Kg );
-VALUES (2, 'Gabumon', '15-10-2018', TRUE, 2, 8); 
+INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_Kg )
+VALUES (2, 'Gabumon', '2018-11-15', true, 2, 8); 
 
-INSERT INTO animals (name, date_of_birth, neutered, escape_attempts, weight_Kg );
-VALUES (3, 'Pikachu', '07-01-2021', FALSE, 1, 15.04); 
+INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_Kg )
+VALUES (3, 'Pikachu', '2021-01-07', false, 1, 15.04); 
 
-INSERT INTO animals (name, date_of_birth, neutered, escape_attempts, weight_Kg );
-VALUES (4, 'Devimon', '12-04-2017', TRUE, 5, 11); 
+INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_Kg )
+VALUES (4, 'Devimon', '2017-05-12', true, 5, 11); 
