c835caafbf35675ab1d00ddce71cf65a0eaf29a6
felixDev22 <felixnzomo@gmail.com>
2023-03-01 00:58:40 +0300

    Update table, add PRIMARY KEY, add species

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT,
+    id INT PRIMARY KEY,
     name VARCHAR(100),
     date_of_birth DATE,
     neutered BOOLEAN,
     escape_attempts INT,
-    weight_Kg DECIMAL(10,2)
+    weight_Kg DECIMAL(10,2),
+    species VARCHAR(255)
 );
