91730c07addda12ce580a13b99a77a395069829d
felixDev22 <felixnzomo@gmail.com>
2023-02-28 09:42:48 +0300

    Populate date in the animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,13 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, neutered, escape_attempts, weight_Kg ); 
+VALUES (1, 'Agumon', '03-02-2020', TRUE, 0, 10.23);
+
+INSERT INTO animals (name, date_of_birth, neutered, escape_attempts, weight_Kg );
+VALUES (2, 'Gabumon', '15-10-2018', TRUE, 2, 8); 
+
+INSERT INTO animals (name, date_of_birth, neutered, escape_attempts, weight_Kg );
+VALUES (3, 'Pikachu', '07-01-2021', FALSE, 1, 15.04); 
+
+INSERT INTO animals (name, date_of_birth, neutered, escape_attempts, weight_Kg );
+VALUES (4, 'Devimon', '12-04-2017', TRUE, 5, 11); 
