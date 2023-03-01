cbe20618aa1f184d87d57686ba084705431581cc
felixDev22 <felixnzomo@gmail.com>
2023-03-01 20:05:47 +0300

    Update schema with owners and species table

--- schema.sql
+++ schema.sql
@@ -1,11 +1,22 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(100),
     date_of_birth DATE,
     neutered BOOLEAN,
     escape_attempts INT,
     weight_Kg DECIMAL(10,2),
     species VARCHAR(255)
+);
+
+CREATE TABLE owners (
+    id BIGSERIAL PRIMARY KEY,
+    Full_name VARCHAR(150) NOT NULL,
+    Age INT NOT NULL
+);
+
+CREATE TABLE species (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(150)
 );
